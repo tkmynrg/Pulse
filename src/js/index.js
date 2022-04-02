c65ce74097f717b75b824a0cc32388b0a88aac6c
Artur Allamyradov <allamuradov@internetlab.ru>
2022-04-03 00:26:20 +0400

    swiper catalog-block

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 import $ from 'jquery';
-
-
+import Swiper, {Navigation, Pagination, Autoplay} from 'swiper';
 import lozad from "lozad";
 
 
@@ -9,4 +8,28 @@ $(function () {
     observer.observe();
 });
 
-
+const swiper = new Swiper('#carousel-swiper', {
+    effect: 'slide',
+    fadeEffect: {
+        crossFade: true
+    },
+    modules: [Navigation, Pagination, Autoplay],
+    loop: false,
+    slidesPerView: 3,
+    spaceBetween: 20,
+    breakpoints: {
+        999: {
+            slidesPerView: 1,
+        },
+    },
+    navigation: {
+        nextEl: '.carousel-button-next',
+        prevEl: '.carousel-button-prev',
+        clickable: true,
+    },
+    pagination: {
+        el: '.carousel-slider-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+});
